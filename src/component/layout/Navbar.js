1b7f421402ce2222fba4c332ee7949e0a448c519
Akash52 <ac8572611@gmail.com>
2020-11-26 18:52:06 +0530

    Functional Com.

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 
-const Navbar=(props)=>  {
+export class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar drop-shadow bg-primary ">
-        <h1>
-          <i className={this.props.icon} />
-          {this.props.title}
-        </h1>
-      </nav>
+      <div>
+        <nav className="navbar drop-shadow bg-primary ">
+          <h1>
+            <i className={this.props.icon} />
+            {this.props.title}
+          </h1>
+        </nav>
+      </div>
     );
   }
 }
