b5ce8f868e7e9921e962e00cbfa140b9fa5fe03c
Akash52 <ac8572611@gmail.com>
2021-04-06 14:10:29 +0530

    Events & Search Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './component/layout/Navbar'
 import Users from './component/layout/users/Users'
 import axios from 'axios'
 import './App.css'
+import Search from './component/layout/users/Search'
 
 class App extends Component {
   state = {
@@ -10,10 +11,12 @@ class App extends Component {
     loading: false,
   }
   async componentDidMount() {
-    console.log(process.env.REACT_CLIENT_KEY)
+    const REACT_CLIENT_ID = 'ccd3a0c757c978538dd4'
+    const REACT_CLIENT_KEY = '05579e08c00b61cae13f066d6b5dc818e8b71842'
+
     this.setState({ loading: true })
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_CLIENT_ID}&client_secret=${process.env.REACT_CLIENT_KEY}`
+      `https://api.github.com/users?client_id=${REACT_CLIENT_ID}&client_secret=${REACT_CLIENT_KEY}`
     )
     this.setState({ users: res.data, loading: false })
   }
@@ -22,6 +25,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
