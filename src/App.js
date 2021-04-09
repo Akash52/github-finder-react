22039d782cdd6ca943843704f82711fcaa052be8
Akash52 <ac8572611@gmail.com>
2021-04-09 15:09:02 +0530

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './component/layout/Navbar'
 import Users from './component/layout/users/Users'
 import axios from 'axios'
 import './App.css'
 import Search from './component/layout/users/Search'
 import Alert from './component/layout/Alert'
+import About from './component/pages/About'
 
 class App extends Component {
   state = {
@@ -48,19 +50,32 @@ class App extends Component {
   render() {
     const { users, loading } = this.state
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
