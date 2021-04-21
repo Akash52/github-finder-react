ed7917dc1200ae1de4bca9b7d471c22e10724738
Akash52 <ac8572611@gmail.com>
2021-04-21 15:40:01 +0530

    Alert Context Workflow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './component/layout/Navbar'
 import Users from './component/layout/users/Users'
@@ -8,56 +8,35 @@ import Search from './component/layout/users/Search'
 import Alert from './component/layout/Alert'
 import About from './component/pages/About'
 import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
 
-let REACT_CLIENT_ID = 'ccd3a0c757c978538dd4'
-let REACT_CLIENT_KEY = '05579e08c00b61cae13f066d6b5dc818e8b71842'
 const App = () => {
-  const [alert, setAlert] = useState(null)
-
-  // async componentDidMount() {
-  //   const REACT_CLIENT_ID = 'ccd3a0c757c978538dd4'
-  //   const REACT_CLIENT_KEY = '05579e08c00b61cae13f066d6b5dc818e8b71842'
-
-  //   this.setState({ loading: true })
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${REACT_CLIENT_ID}&client_secret=${REACT_CLIENT_KEY}`
-  //   )
-  //   this.setState({ users: res.data, loading: false })
-  // }
-
-  // Clear USER from state
-
-  // Set Alert
-
-  const showAlert = (msg, type) => {
-    setAlert({ msg: msg, type: type })
-    setTimeout(() => setAlert(null), 3000)
-  }
-
   return (
     <GithubState>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container">
-            <Alert alert={alert} />
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={(props) => (
-                  <Fragment>
-                    <Search setAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                )}
-              />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/user/:login" component={User} />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={(props) => (
+                    <Fragment>
+                      <Search />
+                      <Users />
+                    </Fragment>
+                  )}
+                />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/user/:login" component={User} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   )
 }
